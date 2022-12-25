d3114f983b2cca9763a58f9c1eb3ca4a1412489b
John Hester <john@imandra.ai>
2022-12-24 21:27:46 -0600

    chore: format mli

--- src/hsplay.mli
+++ src/hsplay.mli
@@ -1,19 +1,19 @@
-module type OrderedType =
-  sig
-    type t
-    val compare: t -> t -> int
-  end
+module type OrderedType = sig
+  type t
+
+  val compare : t -> t -> int
+end
 
 module type S = sig
-    type elt
-    (** The type of the splay tree elements. *)
+  type elt
+  (** The type of the splay tree elements. *)
 
-    type t
-    (** The type of splay trees. *)
+  type t
+  (** The type of splay trees. *)
 
-    val create : unit -> t
+  val create : unit -> t
 
-    val insert : t -> elt -> unit
+  val insert : t -> elt -> unit
 end
 
 module Make (Ord : OrderedType) : S with type elt = Ord.t
