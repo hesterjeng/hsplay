1fb5c327efc787fa0270415c01dcf9fb85bd92c5
John Hester <john@imandra.ai>
2023-01-02 14:56:49 -0600

    feat: add the disable polymorphism module

---
+++ src/disable_polymorphism.ml
@@ -0,0 +1,20 @@
+type do_not_use_polymorphism = Dummy
+
+let ( = ) (_x : do_not_use_polymorphism) (_y : do_not_use_polymorphism) =
+  assert false
+
+let ( <> ) (_x : do_not_use_polymorphism) (_y : do_not_use_polymorphism) =
+  assert false
+
+let compare (_x : do_not_use_polymorphism) (_y : do_not_use_polymorphism) =
+  assert false
+
+let equal_string (x : string) (y : string) = CCString.equal x y
+
+let equal_list = CCList.equal
+
+let equal_bool (x : bool) (y : bool) =
+  match x, y with
+  | true, true -> true
+  | false, false -> true
+  | _ -> false
