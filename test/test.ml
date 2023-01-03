5adb9e131a584ccf6a7127c5ea55a2cbf1b696ea
John Hester <john@imandra.ai>
2023-01-03 14:42:41 -0600

    test: rearrange order of tests

--- test/test.ml
+++ test/test.ml
@@ -3,8 +3,8 @@ let () =
     [
       "init", Splay_test.init_test;
       "splay_once_test", Splay_test.splay_once_test;
+      "splay_twice_test", Splay_test.splay_twice_test;
       "splay_thrice_test", Splay_test.splay_thrice_test;
       "splay_ident_test", Splay_test.splay_ident_test;
-      "splay_twice_test", Splay_test.splay_twice_test;
       "splay_med_test", Splay_test.splay_med_test;
     ]
