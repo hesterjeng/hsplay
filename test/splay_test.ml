63236ef304d22e1c389201b55c8741ad34979aec
John Hester <john@imandra.ai>
2023-01-03 11:18:02 -0600

    test: add some splay tests

--- test/splay_test.ml
+++ test/splay_test.ml
@@ -1,1 +1,55 @@
 let init_test = [ ("init", `Quick, fun _ -> ()) ]
+
+let splay_once_test =
+  [
+    ( "splay_once_test",
+      `Quick,
+      fun _ ->
+        let module S = Hsplay.Make (CCInt) in
+        let s = S.create () in
+        S.insert s 3;
+        S.insert s 4;
+        () );
+  ]
+
+let splay_twice_test =
+  [
+    ( "splay_twice_test",
+      `Quick,
+      fun _ ->
+        let module S = Hsplay.Make (CCInt) in
+        let s = S.create () in
+        S.insert s 3;
+        S.insert s 4;
+        S.insert s 1;
+        () );
+  ]
+
+let splay_thrice_test =
+  [
+    ( "splay_thrice_test",
+      `Quick,
+      fun _ ->
+        let module S = Hsplay.Make (CCInt) in
+        let s = S.create () in
+        S.insert s 3;
+        S.insert s 4;
+        S.insert s 1;
+        S.insert s 0;
+        () );
+  ]
+
+let splay_ident_test =
+  [
+    ( "splay_ident_test",
+      `Quick,
+      fun _ ->
+        let module S = Hsplay.Make (CCInt) in
+        let s = S.create () in
+        S.insert s 3;
+        S.insert s 4;
+        S.insert s 1;
+        S.insert s 0;
+        S.insert s 0;
+        () );
+  ]
--- test/test.ml
+++ test/test.ml
@@ -1,1 +1,9 @@
-let () = Alcotest.run "Hsplay" [ "init", Splay_test.init_test ]
+let () =
+  Alcotest.run "Hsplay"
+    [
+      "init", Splay_test.init_test;
+      "splay_once_test", Splay_test.splay_once_test;
+      "splay_thrice_test", Splay_test.splay_thrice_test;
+      "splay_ident_test", Splay_test.splay_ident_test;
+      "splay_twice_test", Splay_test.splay_twice_test;
+    ]
